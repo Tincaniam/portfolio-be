3be596075f896d21501ca96a917187d989b35ee5
mtinnel <matttinnel@gmail.com>
2022-10-11 11:43:15 -0700

    first commit

---
+++ projects_controller.mjs
@@ -0,0 +1,139 @@
+import 'dotenv/config';
+import * as projects from './projects_model.mjs';
+import express from 'express';
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+/**
+*
+* @param {string} date
+* Return true if the date format is MM-DD-YY where MM, DD and YY are 2 digit integers
+*/
+function isDateValid(date) {
+    const format = /^\d\d-\d\d-\d\d$/;
+    return format.test(date);
+}
+
+/**
+ * Create a new project with the name, status, description, link, and date provided in the body
+ */
+app.post('/projects', (req, res) => {
+    const validStatus = ["In Progress", "Completed"]
+    if (validStatus.includes(req.body.status) !== true || isDateValid(req.body.date) !== true) {
+        console.log.error
+        res.status(400).json({ Error: 'Invalid request'});
+    }
+    else {
+        projects.createProject(req.body.name, req.body.status, req.body.description, req.body.link, req.body.date)
+            .then(project => {
+                res.status(201).json(project);
+            })
+            .catch(error => {
+                console.error(error);
+                res.status(400).json({ Error: 'Invalid request'});
+            });
+    }
+});
+
+
+/**
+ * Retrive the project corresponding to the ID provided in the URL.
+ */
+app.get('/projects/:_id', (req, res) => {
+    const projectId = req.params._id;
+    projects.findProjectById(projectId)
+        .then(project => {
+            if (project !== null) {
+                res.json(project)
+            } else {
+                res.status(404).json({ Error: 'Not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(400).json({ Error: 'Invalid request'});
+        });
+});
+
+/**
+ * Retrieve projects. 
+ */
+app.get('/projects', (req, res) => {
+    const filter = {};
+    if (req.query.name !== undefined){
+        filter.name = (req.query.name)
+    };
+    if (req.query.status !== undefined){
+        filter.status = (req.query.status)
+    };
+    if (req.query.description !== undefined){
+        filter.description = (req.query.description)
+    };
+    if (req.query.link !== undefined){
+        filter.link = (req.query.link)
+    };
+    if (req.query.date !== undefined){
+        filter.date = (req.query.date)
+    };
+
+    projects.findProjects(filter)
+    .then(project => {
+        res.status(200).json(project);
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(400).json({ Error: 'Invalid request'});
+    });
+});
+
+/**
+ * Update the project whose id is provided in the path parameter and set
+ * it to the values provided in the body.
+ */
+ app.put('/projects/:_id', (req, res) => {
+    const validStatus = ["In Progress", "Completed"]
+    if (typeof(req.body.name) !== "string" || (typeof(req.body.description) !== "string" || typeof(req.body.link) !== "string" || validStatus.includes(req.body.status) !== true || isDateValid(req.body.date) !== true)) {
+        console.log.error
+        res.status(400).json({ Error: 'Invalid request'});
+    }
+    else {
+    projects.replaceProject(req.params._id, req.body.name, req.body.status, req.body.description, req.body.link, req.body.date)
+        .then(numUpdated => {
+            if (numUpdated === 1) {
+                res.json({ _id: req.params._id, name: req.body.name, status: req.body.status, description: req.body.description, link: req.body.link, date: req.body.date })
+            } else {
+                res.status(404).json({ Error: 'Not found'});
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(400).json({ Error: 'Invalid request'});
+        });
+    }
+});
+
+/**
+ * Delete the project whose id is provided in the query parameters
+ */
+ app.delete('/projects/:_id', (req, res) => {
+    projects.deleteById(req.params._id)
+        .then(deletedCount => {
+            if (deletedCount === 1) {
+                res.status(204).send();
+            } else {
+                res.status(404).json({ Error: 'Not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.send({ error: 'Invalid request' });
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
---
+++ projects_model.mjs
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+// Prepare to the database running on Mongodb cloud.
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+
+// Connect to the database.
+const db = mongoose.connection;
+
+// The open event is called when the connection is succsesfully opened.
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+
+// Defines the Schema.
+const projectSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    status: {type: String, required: true},
+    description: {type: String, required: true}, 
+    link: {type: String, required: false},
+    date: {type: String, required: true}
+});
+
+// Mongoose Model that creates the Javascript class Project.
+const Project = mongoose.model("Project", projectSchema);
+
+/**
+* Create an Project
+* @param {String} name
+* @param {String} status
+* @param {String} description
+* @param {String} link
+* @param {String} date
+* @returns A promise. Resolves to the JavaScript  for the document created by calling save.
+*/
+const createProject = async(name, status, description, link, date) => {
+    // Call the constructor to create an instance of the model class Project
+    const project = new Project({name: name, status: status, description: description, link: link, date: date});
+    // Call save to persist this object as a document in MongoDB
+    return project.save();
+};
+
+const findProjects = async(filter) => {
+    const query = Project.find(filter);
+    return query.exec();
+};
+
+const findProjectById = async (_id) => {
+    const query = Project.findById(_id);
+    return query.exec();
+}
+
+/**
+* Replace the name, reps, weight, unit,  and date properties of the Exercise with the id value provided.
+* @param {String} _id
+* @param {String} name
+* @param {Number} status
+* @param {Number} description
+* @param {String} link
+* @param {String} date
+* @returns A promise. Resolves to the JavaScript  for the document created by calling save.
+*/
+const replaceProject = async (_id, name, status, description, link, date) => {
+    const result = await Project.replaceOne({ _id: _id}, { name: name, status: status, description: description, link: link, date: date });
+    return result.modifiedCount;
+}
+
+const deleteById = async (_id) => {
+    const result = await Project.deleteOne({ _id: _id });
+    return result.deletedCount;
+}
+
+export {createProject, findProjects, findProjectById, replaceProject, deleteById};
